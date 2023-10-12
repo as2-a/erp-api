73bf7183d4acf48c66cb58ca6a158ee8fa226614
Alessandro Juárez <alessj20@gmail.com>
2023-10-11 20:46:39 -0600

    deploy changes

--- functions/app.js
+++ functions/app.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const serverless = require('serverless-http');
+const userRouter = require('./routes/user.router');
 
 dotenv.config();
 const app = express();
 
-const routerApi = require('./routes');
-
-const port = process.env.PORT || 3000;
-
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send('BANKS-API');
+app.get('/', (req, res) => {
+  res.send('BANKS-API');
 });
 
-routerApi(app);
+app.use('/.netlify/functions/app/user', userRouter);
 
-app.listen(port,()=>{
-    console.log("Listen on port ==> ", port);
-});
+module.exports.handler = serverless(app);
